080e15bec86db532669f05ba92e5a17f61cf25a6
DArtagnant <105509558+DArtagnant@users.noreply.github.com>
2024-03-13 15:42:05 +0100

    nilakantha python improving style

--- python_version/pi_nilakantha.py
+++ python_version/pi_nilakantha.py
@@ -1,13 +1,15 @@
 from decimal import Decimal, getcontext
 
+#Time Complexity: O(N * logN * loglogN) N = reps
+#Auxilary Space : O(1)
+
 def pi_nilakantha(reps):
-    result = Decimal(3.0)
-    op = 1
-    n = 2
+    approx = Decimal(3.0)
+    sign = 1
     for n in range(2, (2 * reps) + 1, 2):
-        result += 4 / Decimal(n * (n + 1) * ( n + 2) * op)
-        op *= -1
-    return result
+        approx += 4 / Decimal(n * (n + 1) * (n + 2) * sign)
+        sign *= -1
+    return approx
 
 if __name__ == "__main__":
     getcontext().prec = 100
