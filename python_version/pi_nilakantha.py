55d0f596e2752d9e4190a9ff333f57edfd847462
DArtagnant <105509558+DArtagnant@users.noreply.github.com>
2024-03-13 15:43:21 +0100

    nilakantha python improving style 2

--- python_version/pi_nilakantha.py
+++ python_version/pi_nilakantha.py
@@ -7,7 +7,7 @@ def pi_nilakantha(reps):
     approx = Decimal(3.0)
     sign = 1
     for n in range(2, (2 * reps) + 1, 2):
-        approx += 4 / Decimal(n * (n + 1) * (n + 2) * sign)
+        approx += sign * 4 / Decimal(n * (n + 1) * (n + 2))
         sign *= -1
     return approx
 
