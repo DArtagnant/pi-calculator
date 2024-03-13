617d22b0737011f13aaa7370e5bf49de44f8fa49
DArtagnant <105509558+DArtagnant@users.noreply.github.com>
2024-03-13 18:33:44 +0100

    test

---
+++ cpp_version/pi_nilakantha.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    std::decimal::decimal64::decimal64 test;
+    
+    cout << fixed << setprecision(50);
+    double myFloat = 0.123456789123456789123456789123456789123456789;
+    cout << myFloat << endl;
+}
