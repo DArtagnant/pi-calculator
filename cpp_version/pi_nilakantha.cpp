a78d6da0365574f71d3c42aff285a71196b918c8
DArtagnant <105509558+DArtagnant@users.noreply.github.com>
2024-03-13 20:34:09 +0000

    c++ pi_nilakantha

--- cpp_version/pi_nilakantha.cpp
+++ cpp_version/pi_nilakantha.cpp
@@ -2,11 +2,21 @@
 #include <iomanip>
 #include <boost/multiprecision/cpp_dec_float.hpp>
 
-const unsigned int DECIMALES = 100;
-typedef boost::multiprecision::cpp_dec_float<DECIMALES> veryLong;
+const unsigned int DECIMALES = 10000;
+using VeryLong = boost::multiprecision::number<boost::multiprecision::cpp_dec_float<DECIMALES>>;
+
+VeryLong pi_nilakantha(VeryLong reps) {
+    VeryLong approx = 3;
+    int sign = 1;
+    for (VeryLong n = 2; n * 2 <= reps; n += 2) {
+        approx += sign * 4 / (n * (n + 1) * (n + 2));
+        sign *= -1;
+    }
+    return approx;
+}
 
 int main() {
     std::cout << std::fixed << std::setprecision(DECIMALES);
-    veryLong myFloat = veryLong("0.123456789123456789123456789123456789123456789");
-    std::cout << myFloat.str(DECIMALES, std::ios_base::dec) << std::endl;
+    VeryLong approx_pi = pi_nilakantha(VeryLong("100000"));
+    std::cout << approx_pi.str(DECIMALES, std::ios_base::dec) << std::endl;
 }
