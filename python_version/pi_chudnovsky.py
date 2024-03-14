670c6f953d45c63a7a048efda7c0ffcd950fab43
DArtagnant <105509558+DArtagnant@users.noreply.github.com>
2024-03-14 08:59:28 +0000

    python pui chudnovsky correc

--- python_version/pi_chudnovsky.py
+++ python_version/pi_chudnovsky.py
@@ -4,9 +4,9 @@ from math import factorial
 def pi_chudnovsky(reps):
     approx = Decimal(0)
     for k in range(0, reps+1):
-        approx += 12 * ( Decimal((-1)**k)*factorial(6*k)*(13591409 + 545140134*k) /
+        approx += ( Decimal((-1)**k)*factorial(6*k)*(13591409 + 545140134*k) /
                    Decimal(factorial(3*k)*(factorial(k)**3)*(640320**((3*k)+(Decimal(3)/2)))) )
-    return 2 * approx
+    return 1 / (12 * approx)
 
 if __name__ == "__main__":
     getcontext().prec = 100
